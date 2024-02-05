032d721f6c7e9c58c1b6bff9542e8412362f22c1
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2024-02-05 10:58:33 +0000

    Prepared files for Task 1, needs testing

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    const amenitiesChecked = {};
+
+    // Listen for changes on each input checkbox tag
+    $('input[type="checkbox"]').change(function () {
+        const amenityId = $(this).data('id');
+        const amenityName = $(this).data('name');
+
+        // Check if the checkbox is checked
+        if ($(this).is(':checked')) {
+            amenitiesChecked[amenityId] = amenityName;
+        } else {
+            delete amenitiesChecked[amenityId];
+        }
+
+        // Update the h4 tag inside the div Amenities with the list of Amenities checked
+        const amenitiesList = Object.values(amenitiesChecked).join(', ');
+        $('.amenities h4').text(amenitiesList);
+    });
+});
