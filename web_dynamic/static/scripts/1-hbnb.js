c0c9ef87d18ae78c9f85e7b865c0449bd6ba6ff3
Eonvorax <mail4bishop@gmail.com>
2024-02-05 12:11:58 +0100

    Fix: global $ and semistandard

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,21 +1,21 @@
+/* global $ */
 $(document).ready(function () {
+  const amenitiesChecked = {};
 
-    const amenitiesChecked = {};
+  // Listen for changes on each input checkbox tag
+  $('input[type="checkbox"]').change(function () {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
 
-    // Listen for changes on each input checkbox tag
-    $('input[type="checkbox"]').change(function () {
-        const amenityId = $(this).data('id');
-        const amenityName = $(this).data('name');
+    // Check if the checkbox is checked
+    if ($(this).is(':checked')) {
+      amenitiesChecked[amenityId] = amenityName;
+    } else {
+      delete amenitiesChecked[amenityId];
+    }
 
-        // Check if the checkbox is checked
-        if ($(this).is(':checked')) {
-            amenitiesChecked[amenityId] = amenityName;
-        } else {
-            delete amenitiesChecked[amenityId];
-        }
-
-        // Update the h4 tag inside the div Amenities with the list of Amenities checked
-        const amenitiesList = Object.values(amenitiesChecked).join(', ');
-        $('.amenities h4').text(amenitiesList);
-    });
+    // Update the h4 tag inside the div Amenities with the list of Amenities checked
+    const amenitiesList = Object.values(amenitiesChecked).join(', ');
+    $('.amenities h4').text(amenitiesList);
+  });
 });
