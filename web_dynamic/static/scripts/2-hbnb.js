908bec6682f7fc20d7d4cb1d463fe2d085ed8728
Eonvorax <mail4bishop@gmail.com>
2024-02-06 16:42:23 +0100

    Red circle works with localhost instead of 0.0.0.0

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -17,11 +17,11 @@ $(document).ready(() => {
     $('.amenities h4').text(Object.values(amenities).join(', '));
   });
 
-  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+  $.get('http://localhost:5001/api/v1/status/', data => {
     if (data.status === 'OK') {
-      $('#api_status').addClass('available');
+      $('div#api_status').addClass('available');
     } else {
-      $('#api_status').removeClass('available');
+      $('div#api_status').removeClass('available');
     }
   });
 });
